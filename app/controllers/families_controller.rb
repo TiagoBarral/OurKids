f7d4f9155892a07649ff5d61ba6bbd2973710307
vasco figueiredo <vaskafig@gmail.com>
2018-12-11 11:26:14 +0000

    add family

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,11 @@
 class FamiliesController < ApplicationController
+  def new
+    @family = Family.new
+  end
+
+  def create
+    @family = Family.new
+    @family.parent = current_user
+    redirect_to families_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'pages#landing'
   devise_for :users
-  root to: 'pages#home'
+  resources :families do
+    resources :children
+    resources :expenses
+    resources :payments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
