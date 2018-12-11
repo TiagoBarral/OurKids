0d6fa0691e2e4917b8540b8b539886c556efa294
Mathilde Bergue <berguemathilde@gmail.com>
2018-12-11 17:46:09 +0000

    modified dashboard and footer

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   devise_for :users
   post '/families/coparent', to: "families#coparent"
   get '/families/coparent', to: 'families#new_coparent'
+  get '/dashboard', to: 'pages#dashboard'
   resources :families, only: [:index, :show, :new, :create] do
     resources :children, only: [:new, :create, :edit, :update]
     resources :expenses
     resources :payments
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
