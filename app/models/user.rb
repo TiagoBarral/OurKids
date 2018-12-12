6e387835fb4f0496ef2c055e5f0b60a217c07424
vasco figueiredo <vaskafig@gmail.com>
2018-12-12 16:39:26 +0000

    cloudinary actually works and displays with default pictures for kids and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  before_create :check_avatar
+  mount_uploader :photo, PhotoUploader
+  after_create :check_avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  mount_uploader :photo, PhotoUploader
   has_many :parent_families, class_name: :Family, foreign_key: :parent_id
   has_many :coparent_families, class_name: :Family, foreign_key: :coparent_id
   has_many :parent_children, through: :parent_families, source: :children
@@ -21,6 +21,6 @@ class User < ApplicationRecord
   end
 
   def check_avatar
-    self.remote_photo_url = 'https://res.cloudinary.com/drpwozhzg/image/upload/v1544626317/default-avatar.png' if self.remote_photo_url.nil?
+    self.remote_photo_url = 'https://res.cloudinary.com/drpwozhzg/image/upload/v1544626317/default-avatar.png' if self.photo.file.nil?
   end
 end
