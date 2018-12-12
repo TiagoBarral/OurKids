8e4684e7f2b8177a74f89c35a9424b6d4687fde2
vasco figueiredo <vaskafig@gmail.com>
2018-12-12 11:46:22 +0000

    redirected on login and signup, added some logic to families index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    families_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
