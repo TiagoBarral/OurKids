cde0289702cf079e09c2f90e38326f090b7db153
Joao Lemos <joaomrlemos@gmail.com>
2018-12-18 17:11:46 +0000

    Change with resource of user story

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,21 +2,21 @@ module ApplicationHelper
   def category_icon(category)
     case category
     when 'Education'
-      return 'https://img.icons8.com/ios/30/000000/flying-motarboard.png'
+      return 'https://img.icons8.com/ios/40/000000/flying-motarboard.png'
     when 'Health'
-      return 'https://img.icons8.com/ios/30/000000/stethoscope.png'
+      return 'https://img.icons8.com/ios/40/000000/stethoscope.png'
     when 'Fun'
-      return 'https://img.icons8.com/ios/30/000000/crazy.png'
+      return 'https://img.icons8.com/ios/40/000000/crazy.png'
     when 'Vacation'
-      return 'https://img.icons8.com/ios/30/000000/sunbathe.png'
+      return 'https://img.icons8.com/ios/40/000000/sunbathe.png'
     when 'Food'
-      return 'https://img.icons8.com/ios/30/000000/food.png'
+      return 'https://img.icons8.com/ios/40/000000/food.png'
     when 'Sport'
-      return 'https://img.icons8.com/ios/30/000000/trampoline-park.png'
+      return 'https://img.icons8.com/ios/40/000000/trampoline-park.png'
     when 'Clothes'
-      return 'https://img.icons8.com/ios/30/000000/clothes.png'
+      return 'https://img.icons8.com/ios/40/000000/clothes.png'
     when 'Others'
-      return 'https://img.icons8.com/ios/30/000000/connection-status-off.png'
+      return 'https://img.icons8.com/ios/40/000000/connection-status-off.png'
     end
   end
 end
